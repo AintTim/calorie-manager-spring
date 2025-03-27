737c2b659a0cbeb6a9894866d73c50081bc3ef31
Timur Ainetdinov <tainetdinov@itoolabs.com>
2025-03-28 01:53:28 +0300

    Initial commit

---
+++ src/main/java/com/ainetdinov/caloriemanagerspring/CalorieManagerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ainetdinov.caloriemanagerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieManagerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalorieManagerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ainetdinov/caloriemanagerspring/CalorieManagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ainetdinov.caloriemanagerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieManagerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
