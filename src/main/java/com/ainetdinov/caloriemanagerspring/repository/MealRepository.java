b02e8cbbac446c3ad291755c77588639a398e938
Timur Ainetdinov <tainetdinov@itoolabs.com>
2025-04-01 00:11:14 +0300

    Added repository annotation

--- src/main/java/com/ainetdinov/caloriemanagerspring/repository/MealRepository.java
+++ src/main/java/com/ainetdinov/caloriemanagerspring/repository/MealRepository.java
@@ -2,10 +2,12 @@ package com.ainetdinov.caloriemanagerspring.repository;
 
 import com.ainetdinov.caloriemanagerspring.model.entity.Meal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface MealRepository extends JpaRepository<Meal, Long> {
 
     List<Meal> findByDateAndUserId(LocalDate date, long userId);
--- src/main/java/com/ainetdinov/caloriemanagerspring/repository/UserRepository.java
+++ src/main/java/com/ainetdinov/caloriemanagerspring/repository/UserRepository.java
@@ -3,10 +3,12 @@ package com.ainetdinov.caloriemanagerspring.repository;
 import com.ainetdinov.caloriemanagerspring.model.entity.User;
 import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByEmail(@Email String email);
 }
